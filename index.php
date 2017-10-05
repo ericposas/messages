94e3d4abe6335e486d5f7a4d3a557c5156596a75
Eric Posas <ericposas06@gmail.com>
2017-10-05 09:39:58 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>Send Eric a Message!</title>
+        <link href='style.css' rel='stylesheet'>
+    </head>
+    <body>
+
+    <?php
+    session_start();
+
+    if(isset($_POST['logout']) && !empty($_POST['logout'])){
+        unset($_SESSION['User']);
+    }
+
+    if(isset($_SESSION['User']) && !empty($_SESSION['User'])){
+        echo "<div class='user-input'>Current User: ".$_SESSION['User']."</div>".
+             "<form method='post' action=" . $_SERVER['PHP_SELF'] . ">".
+                "<input class='user-input' type='submit' name='logout' value='log out username'>".
+             "</form>".
+             "<br>";
+        echo "<form method=\"post\" action=\"messages.php\">";
+    }else{
+        echo "<form method=\"post\" action=\"messages.php\">".
+               "<div class='user-input'>Username:</div>".
+               "<input class='user-input' id=\"username\" type=\"text\" name=\"User\" value=\"\">";
+    }
+    ?>
+
+        <div class="user-input">Message:</div>
+        <textarea class="user-input" id="msg" type="text" name="Msg" value=""></textarea>
+        <div class="user-input" id="after-msg">please enter a message.</div>
+        <br>
+        <br>
+        <input class="user-input" id="submit-index" type="submit" name="submit" value="Send">
+    </form>
+    <script>
+        var form = document.getElementsByTagName('form')[0];
+        var submit = document.getElementById('submit-index');
+        var msg = document.getElementById('msg');
+        var after = document.getElementById('after-msg');
+        msg.addEventListener('input', handleMsgInput);
+        function handleMsgInput() {
+            if(msg.value != ''){
+                submit.style.display = 'block';
+                after.style.display = 'none';
+                msg.classList.add('textarea');
+                msg.classList.remove('user-input');
+            }else{
+                submit.style.display = 'none';
+                after.style.display = 'block';
+                if(msg.classList.contains('textarea'))
+                    msg.classList.remove('textarea');
+                    msg.classList.add('user-input');
+            }
+        }
+        //run once.
+        handleMsgInput();
+    </script>
+
+    <br>
+    <?php
+        if(isset($_SESSION['User']) && !empty($_SESSION['User'])){
+            echo "<button class='user-input' id=\"see-msgs-btn\">See Messages</button>";
+        }
+    ?>
+    <script>
+        var btn = document.getElementById('see-msgs-btn') || null;
+        if(btn){
+            btn.addEventListener('click', function () {
+                window.open('messages.php', '_self');
+            });
+        }
+    </script>
+
+    </body>
+</html>
---
+++ messages.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+
+if(isset($_POST['User']) && !empty($_POST['User'])) {
+    $user = filter_input(INPUT_POST, 'User', FILTER_DEFAULT);
+    $user = trim($user);
+    $_SESSION['User'] = filter_var($user, FILTER_SANITIZE_STRING);
+}
+if(isset($_POST['Msg']) && !empty($_POST['Msg'])) {
+    $msg = filter_input(INPUT_POST, 'Msg', FILTER_DEFAULT);
+    if(trim($msg) != '' && trim($_SESSION['User']) != ''){
+        if (!file_put_contents('msgs.html', "<div class='msg'>" . $_SESSION['User'] . ": " . filter_var($msg, FILTER_SANITIZE_STRING) . "</div>", FILE_APPEND | LOCK_EX)) {
+            echo "<div> Error: could not write to file. </div>";
+        }
+    }
+}?>
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<meta charset='UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<title>Message Log</title>
+<script src="gs.js"></script>
+<link href='style.css' rel='stylesheet'>
+
+<?php
+if(!isset($_SESSION['User']) || empty($_SESSION['User'])) {
+    echo "<style>.msg{ display: none; }#iframe,#preloaded-iframe{ visibility:hidden; }</style>";
+}?>
+
+</head>
+<body>
+<button id='swap-btn'>
+<script>
+    var swap_btn = document.getElementById('swap-btn');
+    swap_btn.addEventListener('click', function () {
+        window.open('index.php', '_self');
+    });
+</script>
+<?php
+if(isset($_SESSION['User']) && !empty($_SESSION['User'])) {
+    echo "<- back";
+    echo "</button><br><br>";
+}else{
+    echo "set username".
+         "</button><br><br>".
+         "<div class='sorry'>Sorry, you need to set a username to see<br> or post a message.</div>";
+}?>
+<iframe id="preloaded-iframe" style="display:none;" src="msgs.html"></iframe>
+<iframe id="iframe" style="display:block;" src="msgs.html"></iframe>
+<script>
+  document.getElementById("preloaded-iframe").onload = function () { this.contentWindow.scrollTo(0, 99999) };
+  document.getElementById("iframe").onload = function () { this.contentWindow.scrollTo(0, 99999) };
+</script>
+<br>
+<br>
+<?php
+if(isset($_SESSION['User']) && !empty($_SESSION['User'])){
+    echo "<form method='post' action=" . $_SERVER['PHP_SELF'] . ">" .
+         "<div class='user-input'>" . "User: " . $_SESSION['User'] . "<span class='small user-input'>type message below:</span></div>" .
+            "<textarea class='textarea' name='Msg'>" .
+            "</textarea><br><br>" .
+            "<div class='send-msg-div'><input class='send-msg-btn' type='submit' name='submit' value='send'></div>" .
+         "</form>" .
+         "<br>" .
+         "<br>";
+}?>
+
+<script>
+  var preloaded = document.getElementById('preloaded-iframe');
+  var iframe = document.getElementById('iframe');
+  var everyNSec = 5;
+  //setInterval(swapSrc, (everyNSec * 1000));
+  setInterval(ajaxReq, (everyNSec * 1000));
+  window.ajax_inprogress = false;
+  function swapSrc() {
+    function swap() {
+      if (preloaded.style.display == 'none') {
+        iframe.src = preloaded.src;
+        iframe.style.display = 'none';
+        preloaded.style.display = 'block';
+      } else {
+        preloaded.src = iframe.src;
+        iframe.style.display = 'block';
+        preloaded.style.display = 'none';
+      }
+    }
+    swap();
+    TweenLite.delayedCall(0.5, swap);
+  }
+  function ajaxReq() {
+    if(window.ajax_inprogress == false) {
+      window.ajax_inprogress = true;
+      var http = new XMLHttpRequest();
+      var url = "data.php";
+      <?php
+      if($msg){
+        echo "var params = 'data=" . $msg . "';";
+      } ?>
+      http.open("POST", url, true);
+      http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+          swapSrc();
+          window.ajax_inprogress = false;
+          var res = document.createElement('div');
+          res.innerHTML = http.response;
+          document.body.appendChild(res);
+          //alert(http.responseText);
+        }
+      }
+      http.send(params);
+    }
+  }
+</script>
